b4a86d33c755a97ff0eac49281c53eec1bee3d9e
root <root@ip-10-1-1-142.ec2.internal>
2022-02-18 18:34:12 +0000

    Modified script for adding 4 EBS volumes greater than 1 GB

--- create_vol.sh
+++ create_vol.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for i in {1..3}
+#Modifying script to create 4 EBS volumes
+for i in {1..4}
 do
 echo "Creating Volume No: $i"
 aws ec2 create-volume --volume-type gp2 --size 1 --availability-zone us-east-1a
